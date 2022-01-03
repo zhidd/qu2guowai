552eb089ca3f0bf9db3e8fad2a5e2521bee50196
zhidd <1292177891@qq.com>
2022-01-03 19:46:17 +0800

    Update entry3point.sh

--- entry3point.sh
+++ entry3point.sh
@@ -10,7 +10,7 @@ cat << EOF > ${DIR_TMP}/heroku.json
 {
     "inbounds": [{
         "port": ${PORT},
-        "protocol": "${VLESS}",
+        "protocol": "${VL}",
         "settings": {
             "clients": [{
                 "id": "${ID}",
